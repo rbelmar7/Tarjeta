61e2b174ad8b01baf22979647c21d66db39b0e76
Ricardo Belmar <spamsbelmar@gmail.com>
2024-10-12 11:13:15 -0300

    first commit

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Tarjeta />
+  </div>
+</template>
+
+<script>
+import Tarjeta from './components/Tarjeta.vue';
+
+export default {
+  components: {
+    Tarjeta
+  }
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+}
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 60vh;
+  font-size: 10px;
+  font-family: sans-serif;
+}
+#app {
+  display: flex;
+  gap: 40px;
+}
+</style>
